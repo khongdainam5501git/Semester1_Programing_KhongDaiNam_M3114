584308822764b47d0859456f2c6250231f4333ef
khongdainam5501git <khongdainam254@gmail.com>
2021-11-26 12:14:42 +0300

    add btvn_2.cpp

---
+++ btvn_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace  std;
+
+ int main(){
+     int n, a, b;
+     float x;
+     cin >> n >> a >> b;
+     x = n / (a - b) ;
+    //  if (x - (n / (a - b)) == 0)
+    //  {
+    //      num_days == x;
+    //  }else{
+    //      num_days == x + 1;
+    //  }
+        
+     printf("%f", x);
+     
+     return 0;
+ 
